94b43c8fc2ced56ef08ec1e9925b7661c58ea313
Philippe Chadenier <phil.chadenier@gmail.com>
2020-01-28 22:10:49 +0100

    Updates

---
+++ precache-manifest.6514563a80f49c45879ab25182c9c3f9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e680861a1408b44ce9624e1cde566aa",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "68f3cd9b4ac32b587406",
+    "url": "/app/static/css/2.2277b44b.chunk.css"
+  },
+  {
+    "revision": "5a457ae1e0d5253b1dc7",
+    "url": "/app/static/css/main.65ac6ec7.chunk.css"
+  },
+  {
+    "revision": "68f3cd9b4ac32b587406",
+    "url": "/app/static/js/2.48522db5.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/app/static/js/2.48522db5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a457ae1e0d5253b1dc7",
+    "url": "/app/static/js/main.07486297.chunk.js"
+  },
+  {
+    "revision": "3850e247d4ac686b7c2f",
+    "url": "/app/static/js/runtime-main.9b05ec13.js"
+  },
+  {
+    "revision": "92a68db7158bbff59319e9f90c46016c",
+    "url": "/app/static/media/coop_cubes.92a68db7.png"
+  },
+  {
+    "revision": "de2e3cb3a81ebc46c55cb2298cad1a44",
+    "url": "/app/static/media/flappy_bird.de2e3cb3.png"
+  },
+  {
+    "revision": "c8772287b57593800fb18ac5197b3c63",
+    "url": "/app/static/media/golf_3d.c8772287.png"
+  },
+  {
+    "revision": "a36629b8bd6bfd17d500f523798bd16f",
+    "url": "/app/static/media/pchadeni.a36629b8.JPG"
+  },
+  {
+    "revision": "f4e7b0662aec8dc258c491efe15d5f40",
+    "url": "/app/static/media/pong.f4e7b066.png"
+  },
+  {
+    "revision": "69d9645d3826a77cf7bc5c3dc80ea672",
+    "url": "/app/static/media/sonic.69d9645d.png"
+  },
+  {
+    "revision": "0399ee116eb5b12747ddd2bd8c5c5e23",
+    "url": "/app/static/media/tower_defense.0399ee11.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa0b8f235bc13090b7ebe8bb6c5f8e42.js"
+  "/app/precache-manifest.6514563a80f49c45879ab25182c9c3f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
