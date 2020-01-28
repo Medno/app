1f5ca70dc9bcd89736ecf81ac2c103e165ab676c
Philippe Chadenier <phil.chadenier@gmail.com>
2020-01-28 22:01:48 +0100

    Updates

---
+++ precache-manifest.aa0b8f235bc13090b7ebe8bb6c5f8e42.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02303b85def9f61a4bfc77d4494e1367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "516f1c0a6dc4234f198e",
+    "url": "/static/css/2.2277b44b.chunk.css"
+  },
+  {
+    "revision": "2dc50731943e8bc2dd87",
+    "url": "/static/css/main.65ac6ec7.chunk.css"
+  },
+  {
+    "revision": "516f1c0a6dc4234f198e",
+    "url": "/static/js/2.7a1e4d4f.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/static/js/2.7a1e4d4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dc50731943e8bc2dd87",
+    "url": "/static/js/main.cced9817.chunk.js"
+  },
+  {
+    "revision": "522434466ab66460f266",
+    "url": "/static/js/runtime-main.b1d5aced.js"
+  },
+  {
+    "revision": "92a68db7158bbff59319e9f90c46016c",
+    "url": "/static/media/coop_cubes.92a68db7.png"
+  },
+  {
+    "revision": "de2e3cb3a81ebc46c55cb2298cad1a44",
+    "url": "/static/media/flappy_bird.de2e3cb3.png"
+  },
+  {
+    "revision": "c8772287b57593800fb18ac5197b3c63",
+    "url": "/static/media/golf_3d.c8772287.png"
+  },
+  {
+    "revision": "a36629b8bd6bfd17d500f523798bd16f",
+    "url": "/static/media/pchadeni.a36629b8.JPG"
+  },
+  {
+    "revision": "f4e7b0662aec8dc258c491efe15d5f40",
+    "url": "/static/media/pong.f4e7b066.png"
+  },
+  {
+    "revision": "69d9645d3826a77cf7bc5c3dc80ea672",
+    "url": "/static/media/sonic.69d9645d.png"
+  },
+  {
+    "revision": "0399ee116eb5b12747ddd2bd8c5c5e23",
+    "url": "/static/media/tower_defense.0399ee11.png"
+  }
+]);
