dee563ad4e90203d88447c79027d4ad47933c735
Philippe Chadenier <phil.chadenier@gmail.com>
2020-01-30 11:50:51 +0100

    Edit home text

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
 	componentDidMount() {
 		document.title = "Home | Philippe Chadenier"
 	}
-	
+
 	render() {
 		const imgSize = 200
 		return (
@@ -14,7 +14,7 @@ class Home extends React.Component {
 				<Image src={require('../static/pchadeni.JPG')} alt="" width={imgSize} height={imgSize} className="img-thumbnail" roundedCircle/>
 				<h2>Hi there, I'm Philippe.</h2>
 				<div style={{margin:'25px 40px'}}>
-					<h3>I'm a French student finishing my senior degree at <b>42 Paris</b>.</h3>
+					<h3>I'm a French student finishing his senior degree at <b>42 Paris</b>.</h3>
 					<h3>I have some skills in <b>C#</b>, <b>Unity</b>, <b>C++</b>, C, Python, Node.js, React.js.</h3>
 					<h3>I really enjoy Unity and I want to go deeper, then I'm currently
 					looking for an end of study internship as a Game Developer.</h3>
@@ -24,7 +24,7 @@ class Home extends React.Component {
 					Do you have any questions ? <a href="mailto: medno.pc@gmail.com">Contact me</a>.
 				</div>
 		</Container>
-		);	
+		);
 	}
 }
 
